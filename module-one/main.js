eb8424e46ec664131d87fd400b1b3b98c7b0b1c8
Caio Batista <caio.batista@laborit.com.br>
2021-08-03 19:23:16 -0300

    finishing module

--- module-one/main.js
+++ module-one/main.js
@@ -4,3 +4,18 @@ function AbraOMenu() {
   .classList
   .add('visible')
 }
+
+function FecheOMenu() {
+  document
+  .querySelector('.mobile-menu-nav')
+  .classList
+  .remove('visible')
+}
+
+function ToggleMenu() {
+  if(document.querySelector('.mobile-menu-nav').classList.contains('visible') == true) {
+    FecheOMenu()
+  } else {
+    AbraOMenu()
+  }
+}

4d5c5f411bdbcac0ec34715d2709694413d0a639
Caio Batista <caio.batista@laborit.com.br>
2021-09-01 18:53:07 -0300

    apply animate to modal

--- module-two/src/App.jsx
+++ module-two/src/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import Button from './components/Button';
 import Header from './components/Header';
 import Modal from './components/Modal'
-import logoImg from './logo.png'
 
 function App() {
+  const [modal, setModal] = useState(false)
+
 
   return (
     <div className="App">
       <Header className="caio"/>
 
-      <Button color="blue">Abra a modal</Button>
+      <Button onClick={()=> setModal(true)} color="blue">Abra a modal</Button>
       <br />
       <br />
-      <Button color="blue">Abra a uma porta</Button>
+      <Button onClick={setModal} color="blue">Abra a uma porta</Button>
       <br />
       <br />
       <Button color="blue">Abra qqer coisa</Button>
 
-      <Modal/>
+      {modal && <Modal visible={modal} onClose={()=> setModal(false)} />}
     </div>
   );
 }
--- module-two/src/components/Modal/index.jsx
+++ module-two/src/components/Modal/index.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 import './style.scss'
 
-function Modal() {
+function Modal(props) {
+  const {visible, onClose} = props
+
+  const [visibilidade, setVisibilidade] = useState('')
+
+  
+  const interceptor = () => {
+    setVisibilidade('saida')
+
+    setTimeout(()=> {
+      onClose() // aqui a modal fecha de verdade
+    }, 1000)
+  }
+
   return (
     <>
-      <div className="modal">
-        <div className="card">
-          <div className="btn-close"></div>
+      {
+        visible && (
+        <div className={`modal ${visibilidade}`}>
+          <div className="card">
+            <div onClick={interceptor} className="btn-close"></div>
+          </div>
         </div>
-      </div>
+        )
+      }
+      {
+        visible && <div onClick={interceptor} className="obfuscator"></div>
+      }
     </>
   )
 }
